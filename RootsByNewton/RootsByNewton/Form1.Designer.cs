12c85d1c47ba0a5d9426c50c93a46a9bdba27b73
Wolodymyr01 <57314082+Wolodymyr01@users.noreply.github.com>
2020-03-17 17:31:24 +0300

    Add files via upload

---
+++ RootsByNewton/RootsByNewton/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace RootsByNewton
+{
+    partial class RootsCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RootsCalculator));
+            this.InputNumber = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.InputPower = new System.Windows.Forms.TextBox();
+            this.Accuracy = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputNumber
+            // 
+            resources.ApplyResources(this.InputNumber, "InputNumber");
+            this.InputNumber.Name = "InputNumber";
+            // 
+            // CalculateButton
+            // 
+            resources.ApplyResources(this.CalculateButton, "CalculateButton");
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateRoot);
+            // 
+            // InputPower
+            // 
+            resources.ApplyResources(this.InputPower, "InputPower");
+            this.InputPower.Name = "InputPower";
+            // 
+            // Accuracy
+            // 
+            resources.ApplyResources(this.Accuracy, "Accuracy");
+            this.Accuracy.Name = "Accuracy";
+            // 
+            // RootsCalculator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Accuracy);
+            this.Controls.Add(this.InputPower);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.InputNumber);
+            this.Name = "RootsCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputNumber;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.TextBox InputPower;
+        private System.Windows.Forms.TextBox Accuracy;
+    }
+}
+
---
+++ RootsByNewton/RootsByNewton/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Math;
+// Created by Uladzimir Mileika
+namespace RootsByNewton
+{
+    public partial class RootsCalculator : Form
+    {
+        public RootsCalculator()
+        {
+            InitializeComponent();
+        }
+        private void CalculateRoot(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(InputPower.Text);
+                double A = double.Parse(InputNumber.Text);
+                double eps = double.Parse(Accuracy.Text);
+                double x = (Function(A, n, A) * SecondDerivative(A, n) > 0) ? A : eps;
+                while (Abs(Function(x, n, A)) > eps)
+                {
+                    x -= Function(x, n, A) / Derivative(x, n);
+                }
+                MessageBox.Show($"Result (Newton method): {n}√{A} = {x.ToString()}" +
+                    $"\n Result (FCL function): {n}√{A} = {Pow(A, (double)1/n)}");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Wrong format. Enter integer 'n' and don't use any " +
+                    "non-numeric characters in the upper textbox.");
+            }
+        }
+        public double Function(double x, double n, double A)
+        {
+            return Pow(x, n) - A;
+        }
+        public double Derivative(double x, double n)
+        {
+            return n * Pow(x, n - 1);
+        }
+        public double SecondDerivative(double x, double n)
+        {
+            return n * (n - 1) * Pow(x, n - 2);
+        }
+    }
+}
